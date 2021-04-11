5f797f356fb095914b63366c40604ea7da750bd4
jks7743 <jks7743@rit.edu>
2021-04-10 22:32:04 -0500

    new docs

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BrowserSystem"},{"l":"CommandLineInterface"},{"l":"Search"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BrowserSystem"},{"l":"Commands"},{"l":"GUI"},{"l":"MusicBrainz"},{"l":"Search"}];updateSearchResults();
